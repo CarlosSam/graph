aed223626e740dbbf9f75b58fc65a2dbb1a12e64
alisson <alisson.nunes@gmail.com>
2021-01-03 07:49:49 -0300

    more test cases

--- tests/test_depth_first_search.py
+++ tests/test_depth_first_search.py
@@ -1,6 +1,6 @@
 import unittest
 from node import Node
-from tests.graph_info import simple_graph_info, graph1_info
+from tests.graph_info import simple_graph_info, graph1_info, graph2_info, graph3_info, graph4_info, graph5_info
 
 def loadGraphRecursive(node, info):
     node_info = next(filter(lambda current_node_info: current_node_info['id'] == node.name, info['nodes']))
@@ -28,6 +28,22 @@ class TestDepthFirstSearch(unittest.TestCase):
     def test_graph1(self):
         graph1 = loadGraph(graph1_info)
         self.assertEqual(graph1.depthFirstSearch(), ['A', 'B', 'E', 'F', 'I', 'J', 'C', 'D', 'G', 'K', 'H'])
+
+    def test_graph2(self):
+        graph2 = loadGraph(graph2_info)
+        self.assertEqual(graph2.depthFirstSearch(), ['A','B','D','C'])
+
+    def test_graph3(self):
+        graph3 = loadGraph(graph3_info)
+        self.assertEqual(graph3.depthFirstSearch(), ['A','B','C','F','D','E'])
+
+    def test_graph4(self):
+        graph4 = loadGraph(graph4_info)
+        self.assertEqual(graph4.depthFirstSearch(), ['A', 'B', 'C', 'D','F','E'])
+
+    def test_graph5(self):
+        graph5 = loadGraph(graph5_info)
+        self.assertEqual(graph5.depthFirstSearch(), ['A',"B","G","H","O","P","T","U","Q","R",'V',"W","X",'Z',"Y","I","C",'J',"D", "K",'S',"L", "E", "F", "M","N"])
 
 
 if __name__ == '__main__':
